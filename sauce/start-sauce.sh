9cfd485727e7aae9c625dfb0c54875986c11d07b
Greg Magolan <gmagolan@gmail.com>
2019-02-12 11:16:26 -0800

    add start-sauce.sh script

---
+++ sauce/start-sauce.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x -u -e -o pipefail
+
+# Command arguments that will be passed to sauce-connect.
+sauceArgs=""
+
+if [[ ! -z "${SAUCE_READY_FILE:-}" ]]; then
+  sauceArgs="${sauceArgs} --readyfile ${SAUCE_READY_FILE}"
+fi
+
+if [[ ! -z "${SAUCE_PID_FILE:-}" ]]; then
+  mkdir -p $(dirname ${SAUCE_PID_FILE})
+  sauceArgs="${sauceArgs} --pidfile ${SAUCE_PID_FILE}"
+fi
+
+if [[ ! -z "${SAUCE_TUNNEL_IDENTIFIER:-}" ]]; then
+  sauceArgs="${sauceArgs} --tunnel-identifier ${SAUCE_TUNNEL_IDENTIFIER}"
+fi
+
+echo "Starting Sauce Connect. Passed arguments: ${sauceArgs}"
+
+sc -u ${SAUCE_USERNAME} -k ${SAUCE_ACCESS_KEY} ${sauceArgs}
