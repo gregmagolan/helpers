7549a094b73baf4f19811e8f078fe60de8757751
gregmagolan <gmagolan@gmail.com>
2019-02-11 17:46:10 -0800

    Add test_tag_filters to rules_nodejs test_all on Windows

--- rules_nodejs/test_all.sh
+++ rules_nodejs/test_all.sh
@@ -4,13 +4,30 @@ set -eux -o pipefail
 # -x: shows the commands that get run
 # -o pipefail: causes a pipeline to produce a failure return code if any command errors
 
+# Check environment
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    MSYS_NT*)   machine=Windows;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo "Running on $machine"
+
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd)
 
 $SCRIPT_DIR/clean_all.sh
 
 bazel run @nodejs//:yarn
 bazel build ...
-bazel test ...
+
+if [[ $machine == "Windows" ]] ; then
+    bazel test ... --test_tag_filters=-fix-windows
+else
+    bazel test ...
+fi
 
 # These targets should run
 bazel run //internal/node/test:no_deps
